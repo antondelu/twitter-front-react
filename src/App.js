5b6577f306783d07366c778d9704f30865be1e89
Rodrigo Moreira <roke3tres@gmail.com>
2022-09-01 09:58:53 -0300

    Creación LeftSidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { Sedibar } from "./components/Sedibar.jsx";
+// import { Sedibar } from "./components/Sedibar.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { LeftSidebar } from "./components/LeftSidebar.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Sedibar />
+    <div className="App bg-black">
+      <div className="row">
+        <LeftSidebar />
+      </div>
     </div>
   );
 }
