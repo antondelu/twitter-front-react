c793e6524b4f18109d2ac0952ccbd0668e8b8a5c
Alan K <alankatz2000@gmail.com>
2022-09-01 10:48:17 -0300

    estilos home

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import MiddleHome from "./components/MiddleHome";
 
 function App() {
   return (
-    <div className="App container">
-      <div className="row">
-        <LeftSidebar />
-        <MiddleHome />
-        <div className="col-3 "> </div>
+    <div className="">
+      <div className="App bg-black">
+        <div className="row">
+          <LeftSidebar />
+          <MiddleHome />
+          <div className="col-3 "> </div>
+        </div>
       </div>
     </div>
   );
