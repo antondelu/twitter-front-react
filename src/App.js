b359547778483422c557e75bc78d2e1d0fe6ebfd
Alan K <alankatz2000@gmail.com>
2022-09-01 10:00:06 -0300

    HomeMiddle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Sedibar } from "./components/Sedibar.jsx";
 import "./App.css";
+import MiddleHome from "./components/MiddleHome";
 
 function App() {
   return (
-    <div className="App">
-      <Sedibar />
+    <div className="App container">
+      <div className="row">
+        <div className="col-3"> </div>
+        <MiddleHome />
+        <div className="col-3 "> </div>
+      </div>
     </div>
   );
 }
